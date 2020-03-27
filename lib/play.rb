eee5fbbfc7264436ffbc4d0b7a3393f6c202d075
James Jensen <jnjensen@live.com>
2020-03-27 01:38:13 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -36,6 +36,10 @@ def turn(board)
 end
 
 # Define your play method below
-def play
-  puts "Welcome to Tic Tac Toe!"
-  display_board(board)
+def play(board)
+  player_turn = 1
+  until player_turn == 10
+     turn(board)
+     player_turn += 1
+  end
+end
